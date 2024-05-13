0f181e2608577637621237a9f3d803fb85e6dc39
fayssal el mofatiche <fayssal.elmofatiche@dnbf.tech>
2024-05-13 15:00:25 +0200

    integrate flask-login

--- app/models.py
+++ app/models.py
@@ -4,8 +4,10 @@ import sqlalchemy.orm as so
 from datetime import datetime, timezone
 from app import db
 from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin
+from app import login
 
-class User(db.Model):
+class User(UserMixin, db.Model):
     id: so.Mapped[int] = so.mapped_column(primary_key=True)
     username: so.Mapped[str] = so.mapped_column(sa.String(64), index=True,
                                                 unique=True)
@@ -36,3 +38,8 @@ class Post(db.Model):
 
     def __repr__(self):
         return f'<Post {self.body}>'
+    
+
+@login.user_loader
+def load_user(id):
+    return db.session.get(User, int(id))
