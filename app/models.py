9379fb8b48d42c75819242605ee037d62ee00463
KFWFFM\E831 <fayssal.el_mofatiche_extern@kfw.de>
2024-05-13 15:45:54 +0200

    add login and poetry changes

--- app/models.py
+++ app/models.py
@@ -7,6 +7,7 @@ from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
 from app import login
 
+
 class User(UserMixin, db.Model):
     id: so.Mapped[int] = so.mapped_column(primary_key=True)
     username: so.Mapped[str] = so.mapped_column(sa.String(64), index=True,
@@ -21,13 +22,14 @@ class User(UserMixin, db.Model):
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
-    
+
     def set_password(self, password):
         self.password_hash = generate_password_hash(password)
 
     def check_password(self, password):
         return check_password_hash(pwhash=self.password_hash, password=password)
-    
+
+
 class Post(db.Model):
     id: so.Mapped[int] = so.mapped_column(primary_key=True)
     body: so.Mapped[str] = so.mapped_column(sa.String(140))
@@ -38,7 +40,7 @@ class Post(db.Model):
 
     def __repr__(self):
         return f'<Post {self.body}>'
-    
+
 
 @login.user_loader
 def load_user(id):
