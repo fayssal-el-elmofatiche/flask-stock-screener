176c8939ab0e19823f314b343e4a5ad6a83fc046
fayssal el mofatiche <fayssal.elmofatiche@dnbf.tech>
2024-06-16 23:09:47 +0200

    add explicit context processor for flask shell

--- stockscreener.py
+++ stockscreener.py
@@ -1,1 +1,15 @@
-from app import app
+import sqlalchemy as sa
+import sqlalchemy.orm as so 
+from app import app, db
+from app.models import User, Stock, StockScreener
+
+@app.shell_context_processor
+def make_shell_context():
+    return {
+        'sa': sa,
+        'db': db,
+        'so': so,
+        'User': User,
+        'Stock': Stock,
+        'StockScreener': StockScreener
+    }
