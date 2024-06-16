2a96eb1b85e74a84130e514821b491a420a50841
fayssal el mofatiche <fayssal.elmofatiche@dnbf.tech>
2024-06-16 18:22:18 +0200

    add sqlalchemy wrapper and db

--- config.py
+++ config.py
@@ -1,5 +1,11 @@
 import os
 
+basedir = os.path.abspath(os.path.dirname(__file__))
+
 class Config:
     # Used for example by Flask-WTF to generate CSRF tokens
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
+
+    # SQLAlchemy database URL
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+        'sqlite:///' + os.path.join(basedir, 'app.db')
