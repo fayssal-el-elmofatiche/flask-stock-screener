7ec85e8cf52770b8053b8446280b1fc01e293458
fayssal el mofatiche <fayssal.elmofatiche@dnbf.tech>
2024-06-16 18:38:52 +0200

    add first db models. add migrations

---
+++ app/models.py
@@ -0,0 +1,53 @@
+from typing import Optional
+import sqlalchemy as sa
+import sqlalchemy.orm as so
+from app import db
+
+class User(db.Model):
+    id: so.Mapped[int] = so.mapped_column(primary_key=True)
+    username: so.Mapped[str] = so.mapped_column(sa.String(80), index=True, unique=True, nullable=False)
+    email: so.Mapped[str] = so.mapped_column(sa.String(120), index=True, unique=True, nullable=False)
+    password_hash: so.Mapped[Optional[str]] = so.mapped_column(sa.String(256))
+
+    def __repr__(self):
+        return f"<User('{self.username}')>"
+    
+class Stock(db.Model):
+    id: so.Mapped[int] = so.mapped_column(primary_key=True)
+    symbol: so.Mapped[str] = so.mapped_column(sa.String(10), index=True, unique=True, nullable=False)
+    name: so.Mapped[str] = so.mapped_column(sa.String(100), index=True, nullable=False)
+
+    def __repr__(self):
+        return f"<Stock('{self.symbol}')>"
+
+class StockScreener(db.Model):
+    id: so.Mapped[int] = so.mapped_column(primary_key=True)
+    user_id: so.Mapped[int] = so.mapped_column(sa.ForeignKey('user.id'), nullable=False)
+    stock_id: so.Mapped[int] = so.mapped_column(sa.ForeignKey('stock.id'), nullable=False)
+    date: so.Mapped[str] = so.mapped_column(sa.String(10), nullable=False)
+    price: so.Mapped[float] = so.mapped_column(sa.Float, nullable=False)
+    volume: so.Mapped[int] = so.mapped_column(sa.Integer, nullable=False)
+
+    def __repr__(self):
+        return f"<StockScreener('{self.stock_id}', '{self.date}', '{self.price}', '{self.volume}')>"
+    
+    @classmethod
+    def get_by_stock(cls, stock_id: int) -> Optional['StockScreener']:
+        return cls.query.filter_by(stock_id=stock_id).first()
+    
+    @classmethod
+    def get_by_user(cls, user_id: int) -> Optional['StockScreener']:
+        return cls.query.filter_by(user_id=user_id).first()
+    
+    @classmethod
+    def get_by_user_and_stock(cls, user_id: int, stock_id: int) -> Optional['StockScreener']:
+        return cls.query.filter_by(user_id=user_id, stock_id=stock_id).first()
+    
+    @classmethod
+    def get_by_user_and_date(cls, user_id: int, date: str) -> Optional['StockScreener']:
+        return cls.query.filter_by(user_id=user_id, date=date).first()
+    
+    @classmethod
+    def get_by_user_and_stock_and_date(cls, user_id: int, stock_id: int, date: str) -> Optional['StockScreener']:
+        return cls.query.filter_by(user_id=user_id, stock_id=stock_id, date=date).first()
+    
