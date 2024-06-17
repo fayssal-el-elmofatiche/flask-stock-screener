c8b4de938cfc5ee50d6ae4fcca6c30f3ad5ab08c
fayssal el mofatiche <fayssal.elmofatiche@dnbf.tech>
2024-06-17 22:57:40 +0200

    fix user_mixin

--- app/models.py
+++ app/models.py
@@ -9,7 +9,7 @@ from app import db, login
 def load_user(user_id):
     return db.session.get(User, user_id)
 
-class User(db.Model, db.Model):
+class User(UserMixin, db.Model):
     id: so.Mapped[int] = so.mapped_column(primary_key=True)
     username: so.Mapped[str] = so.mapped_column(sa.String(80), index=True, unique=True, nullable=False)
     email: so.Mapped[str] = so.mapped_column(sa.String(120), index=True, unique=True, nullable=False)
